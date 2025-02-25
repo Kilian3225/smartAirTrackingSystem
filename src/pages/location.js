df3e8d5571b82c6d868ed66a07cdb971fe9e9184
Kilian Zimmermann <kilian.zimmermann@edu.htl-klu.at>
2025-02-25 14:19:21 +0100

    25.02. Grafana Dashboards dynamisch implementiert

--- src/pages/location.js
+++ src/pages/location.js
@@ -36,12 +36,27 @@ const aggregationTypeElement = document.getElementById('aggregationType');
 timeRangeElement.addEventListener('change', updateGrafanaUrl)
 aggregationTypeElement.addEventListener('change', updateGrafanaUrl)
 
-
+function timeRangeToMilliseconds(range) {
+    const now = Date.now();
+    const timeMap = {
+        '1h': 3600000,
+        '6h': 21600000,
+        '12h': 43200000,
+        '24h': 86400000,
+        '2d': 172800000,
+        '7d': 604800000,
+        '30d': 2592000000
+    };
+    return timeMap[range] ? now - timeMap[range] : now - 86400000; // Standard: 1 Tag
+}
 
 function updateGrafanaUrl(event)
 {
     const timeRange = timeRangeElement.value;
     const aggregationType = aggregationTypeElement.value;
+
+    const fromTime = timeRangeToMilliseconds(timeRange);
+    const toTime = Date.now();
 
     // const sessionStorage.getItem('selectedLocationIndex');
     let iframeSelector = '';
@@ -59,12 +74,25 @@ function updateGrafanaUrl(event)
 
         url.searchParams.set('var-zeitbereich', timeRange);
         url.searchParams.set('var-aggregationsType', aggregationType);
-
         url.searchParams.set('var-locationId', (parseInt(selectedLocationIndex)+1).toString());
+        url.searchParams.set('from', fromTime.toString());
+        url.searchParams.set('to', toTime.toString());
 
         iframe.src = url.toString();
         console.log("neue src:", iframe.src);
     })
 }
 
+window.onload = () => {
+    console.log("window.onload ausgelöst");
+
+    if (!timeRangeElement || !aggregationTypeElement) {
+        console.error("timeRangeElement oder aggregationTypeElement nicht gefunden");
+        return;
+    }
+
+    console.log("updateGrafanaUrl wird aufgerufen...");
+    updateGrafanaUrl(new Event('change'));
+    console.log("updateGrafanaUrl wurde aufgerufen");
+};
 
